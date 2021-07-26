c52382289bd1ac02c70b265a52a242af43123708
Mellyza Rismawan <mellyza724@gmail.com>
2021-07-26 11:26:29 +0700

    add payment notification request & response

--- request.go
+++ request.go
@@ -17,11 +17,35 @@ type CreateVaRequest struct {
 }
 
 type InquiryRequest struct {
-	RequestUUID    string `json:"rq_uuid" valid:"required"`
+	RequestUUID     string `json:"rq_uuid" valid:"required"`
 	RequestDateTime string `json:"rq_datetime" valid:"required"`
 	MemberId        string `json:"member_id"`
 	MerchantCode    string `json:"comm_code" valid:"required"`
 	OrderId         string `json:"order_id" valid:"required"`
 	Password        string `json:"password"`
 	Signature       string `json:"signature" valid:"required"`
+}
+
+type PaymentNotificationRequest struct {
+	RequestUUID                string `json:"rq_uuid" valid:"required"`
+	RequestDateTime            string `json:"rq_datetime" valid:"required"`
+	Password                   string `json:"password"`
+	Signature                  string `json:"signature" valid:"required"`
+	MemberId                   string `json:"member_id"`
+	MerchantCode               string `json:"comm_code" valid:"required"`
+	OrderId                    string `json:"order_id" valid:"required"`
+	Ccy                        string `json:"ccy" valid:"required"`
+	Amount                     string `json:"amount" valid:"required"`
+	DebitFromBank              string `json:"debit_from_bank" valid:"required"`
+	DebitFrom                  string `json:"debit_from"`
+	DebitFromName              string `json:"debit_from_name"`
+	CreditToBank               string `json:"credit_to_bank" valid:"required"`
+	CreditTo                   string `json:"credit_to"`
+	CreditToName               string `json:"credit_to_name"`
+	ProductCode                string `json:"product_code" valid:"required"`
+	Message                    string `json:"message"`
+	PaymentDateTime            string `json:"payment_datetime" valid:"required"`
+	PaymentRef                 string `json:"payment_ref" valid:"required"`
+	ApprovalCodeFullBca        string `json:"approval_code_full_bca"`
+	ApprovalCodeInstallmentBca string `json:"approval_code_installment_bca"`
 }
--- response.go
+++ response.go
@@ -15,34 +15,45 @@ type CreateVaResponse struct {
 }
 
 type InquiryResponse struct {
-	RequestUUID     string `json:"rq_uuid" valid:"required"`
-	RequestDateTime string `json:"rq_datetime" valid:"required"`
-	ErrorCode       string  `json:"error_code" valid:"required"`
-	ErrorMessage    string  `json:"error_message" valid:"required"`
-	Signature    string  `json:"signature" valid:"required"`
-	OrderId         string  `json:"order_id"`
-	Amount          float64 `json:"amount"`
-	Ccy             string  `json:"ccy" `
-	Description     string  `json:"desc"`
-	TransactionDate string  `json:"trx_date"`
-	InstallmentPeriod string `json:"installment_period"`
-	CustomerDetails CustomerDetails
-	ShippingAddress ShippingAddress
+	RequestUUID       string  `json:"rq_uuid" valid:"required"`
+	RequestDateTime   string  `json:"rq_datetime" valid:"required"`
+	ErrorCode         string  `json:"error_code" valid:"required"`
+	ErrorMessage      string  `json:"error_message" valid:"required"`
+	Signature         string  `json:"signature" valid:"required"`
+	OrderId           string  `json:"order_id"`
+	Amount            float64 `json:"amount"`
+	Ccy               string  `json:"ccy" `
+	Description       string  `json:"desc"`
+	TransactionDate   string  `json:"trx_date"`
+	InstallmentPeriod string  `json:"installment_period"`
+	CustomerDetails   CustomerDetails
+	ShippingAddress   ShippingAddress
+}
+
+type PaymentNotificationResponse struct {
+	RequestUUID       string `json:"rq_uuid" valid:"required"`
+	ResponseDateTime  string `json:"rs_datetime" valid:"required"`
+	ErrorCode         string `json:"error_code"`
+	ErrorMessage      string `json:"error_message" valid:"required"`
+	Signature         string `json:"signature" valid:"required"`
+	OrderId           string `json:"order_id"`
+	ReconcileID       string `json:"reconcile_id" valid:"required"`
+	ReconcileDateTime string `json:"reconcile_datetime" valid:"required"`
 }
 
 type CustomerDetails struct {
 	FirstName string `json:"firstname" valid:"required"`
-	LastName string `json:"lastname"`
-	Phone string `json:"phone_number" valid:"required"`
-	Email string `json:"email" valid:"required"`
+	LastName  string `json:"lastname"`
+	Phone     string `json:"phone_number" valid:"required"`
+	Email     string `json:"email" valid:"required"`
 }
 
 type ShippingAddress struct {
-	FirstName string `json:"firstname" valid:"required"`
-	LastName string `json:"lastname"`
-	Address string `json:"address" valid:"required"`
-	City string `json:"city" valid:"required"`
-	PostalCode string `json:"postal_code" valid:"required"`
-	Phone string `json:"phone_number" valid:"required"`
+	FirstName   string `json:"firstname" valid:"required"`
+	LastName    string `json:"lastname"`
+	Address     string `json:"address" valid:"required"`
+	City        string `json:"city" valid:"required"`
+	PostalCode  string `json:"postal_code" valid:"required"`
+	Phone       string `json:"phone_number" valid:"required"`
 	CountryCode string `json:"country_code"  valid:"required"`
 }
