bff8455dd189460acb9ccde3eda82d15c763c886
Mellyza Rismawan <mellyza724@gmail.com>
2021-07-29 10:58:44 +0700

    fix missing assign struct

--- request.go
+++ request.go
@@ -51,7 +51,7 @@ type InquiryRequest struct {
 type PaymentNotificationRequest struct {
 	RequestUUID                string `json:"rq_uuid" schema:"rq_uuid" valid:"required"`
 	RequestDateTime            string `json:"rq_datetime" schema:"rq_datetime" valid:"required"`
-	Password                   string `json:"password" schema:"signature"`
+	Password                   string `json:"password" schema:"password"`
 	Signature                  string `json:"signature" schema:"signature" valid:"required"`
 	MemberID                   string `json:"member_id" schema:"member_id"`
 	MerchantCode               string `json:"comm_code" schema:"comm_code" valid:"required"`
--- response.go
+++ response.go
@@ -19,44 +19,44 @@ type CreateVaResponse struct {
 // createInquiryResponseBody Modify InquiryResponse when  you change this struct
 func createInquiryResponseBody(req InquiryResponse) (values url.Values) {
 	values = url.Values{}
-	values.Set("rq_uuid",req.RequestUUID)
-	values.Set("rq_datetime",req.RequestDateTime)
-	values.Set("error_code",req.ErrorCode)
-	values.Set("error_message",req.ErrorMessage)
-	values.Set("signature",req.Signature)
-	values.Set("order_id",req.OrderId)
-	values.Set("amount",req.Amount)
-	values.Set("ccy",req.Ccy)
-	values.Set("description",req.Description)
-	values.Set("trx_date",req.TransactionDate)
-	values.Set("installment_period",req.InstallmentPeriod)
-	values.Set("customer_details.firstname",req.CustomerDetails.FirstName)
-	values.Set("customer_details.lastname",req.CustomerDetails.LastName)
-	values.Set("customer_details.phone_number",req.CustomerDetails.Phone)
-	values.Set("customer_details.email",req.CustomerDetails.Email)
-	values.Set("shipping_address.first_name",req.ShippingAddress.FirstName)
-	values.Set("shipping_address.lastname",req.ShippingAddress.LastName)
-	values.Set("shipping_address.address",req.ShippingAddress.Address)
-	values.Set("shipping_address.city",req.ShippingAddress.City)
-	values.Set("shipping_address.postal_code",req.ShippingAddress.PostalCode)
-	values.Set("shipping_address.phone",req.ShippingAddress.Phone)
-	values.Set("shipping_address.country_code",req.ShippingAddress.CountryCode)
+	values.Set("rq_uuid", req.RequestUUID)
+	values.Set("rq_datetime", req.RequestDateTime)
+	values.Set("error_code", req.ErrorCode)
+	values.Set("error_message", req.ErrorMessage)
+	values.Set("signature", req.Signature)
+	values.Set("order_id", req.OrderId)
+	values.Set("amount", req.Amount)
+	values.Set("ccy", req.Ccy)
+	values.Set("description", req.Description)
+	values.Set("trx_date", req.TransactionDate)
+	values.Set("installment_period", req.InstallmentPeriod)
+	values.Set("customer_details.firstname", req.CustomerDetails.FirstName)
+	values.Set("customer_details.lastname", req.CustomerDetails.LastName)
+	values.Set("customer_details.phone_number", req.CustomerDetails.Phone)
+	values.Set("customer_details.email", req.CustomerDetails.Email)
+	values.Set("shipping_address.first_name", req.ShippingAddress.FirstName)
+	values.Set("shipping_address.lastname", req.ShippingAddress.LastName)
+	values.Set("shipping_address.address", req.ShippingAddress.Address)
+	values.Set("shipping_address.city", req.ShippingAddress.City)
+	values.Set("shipping_address.postal_code", req.ShippingAddress.PostalCode)
+	values.Set("shipping_address.phone", req.ShippingAddress.Phone)
+	values.Set("shipping_address.country_code", req.ShippingAddress.CountryCode)
 	return
 }
 
 // InquiryResponse Modify createInquiryResponseBody when  you change this struct
 type InquiryResponse struct {
-	RequestUUID       string  `json:"rq_uuid" valid:"required"`
-	RequestDateTime   string  `json:"rq_datetime" valid:"required"`
-	ErrorCode         string  `json:"error_code" valid:"required"`
-	ErrorMessage      string  `json:"error_message" valid:"required"`
-	Signature         string  `json:"signature" valid:"required"`
-	OrderId           string  `json:"order_id"`
+	RequestUUID       string `json:"rq_uuid" valid:"required"`
+	RequestDateTime   string `json:"rq_datetime" valid:"required"`
+	ErrorCode         string `json:"error_code" valid:"required"`
+	ErrorMessage      string `json:"error_message" valid:"required"`
+	Signature         string `json:"signature" valid:"required"`
+	OrderId           string `json:"order_id"`
 	Amount            string `json:"amount"`
-	Ccy               string  `json:"ccy" `
-	Description       string  `json:"desc"`
-	TransactionDate   string  `json:"trx_date"`
-	InstallmentPeriod string  `json:"installment_period"`
+	Ccy               string `json:"ccy" `
+	Description       string `json:"desc"`
+	TransactionDate   string `json:"trx_date"`
+	InstallmentPeriod string `json:"installment_period"`
 	CustomerDetails   CustomerDetails
 	ShippingAddress   ShippingAddress
 }
@@ -66,7 +66,7 @@ type PaymentNotificationResponse struct {
 	ResponseDateTime  string `json:"rs_datetime" valid:"required"`
 	ErrorCode         string `json:"error_code"`
 	ErrorMessage      string `json:"error_message" valid:"required"`
-	Signature         string `json:"signature" valid:"required"`
+	Signature         string `json:"signature"`
 	OrderId           string `json:"order_id"`
 	ReconcileID       string `json:"reconcile_id" valid:"required"`
 	ReconcileDateTime string `json:"reconcile_datetime" valid:"required"`
