446e8e719ef66861861844cf66d8046459ba096e
Sactio Swastioyono <email.tyo@gmail.com>
2021-07-30 15:34:36 +0700

    Added valid

--- espay/inquiry.go
+++ espay/inquiry.go
@@ -8,13 +8,13 @@ import (
 )
 
 type InquiryRequest struct {
-	RequestUUID     string `schema:"rq_uuid, required"`
-	RequestDateTime string `schema:"rq_datetime, required"`
-	MemberId        string `schema:"member_id"`
-	MerchantCode    string `schema:"comm_code, required"`
-	OrderId         string `schema:"order_id, required"`
-	Password        string `schema:"password"`
-	Signature       string `schema:"signature, required"`
+	RequestUUID     string `schema:"rq_uuid" valid:"required"`
+	RequestDateTime string `schema:"rq_datetime" valid:"required"`
+	MemberId        string `schema:"member_id" valid:"-"`
+	MerchantCode    string `schema:"comm_code" valid:"required"`
+	OrderId         string `schema:"order_id" valid:"required"`
+	Password        string `schema:"password" valid:"-"`
+	Signature       string `schema:"signature" valid:"required"`
 }
 
 // createInquiryResponseBody Modify InquiryResponse when  you change this struct
